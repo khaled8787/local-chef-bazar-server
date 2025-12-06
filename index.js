9a38299458bd4dc446bde868963bdff58ea3695e
khaled8787 <khaledmahmud01737@gmail.com>
2025-12-06 23:29:49 +0600

    meals in the home page addeded

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express')
+const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
 const cors = require('cors');
-const port = process.env.PORT || 3000
+require('dotenv').config();
 
-require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@practice-mongo.h6y3sbv.mongodb.net/?appName=Practice-mongo`;
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,22 +21,37 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
+    const db = client.db("LocalChefBazar");
+    const mealsCollection = db.collection("meals");
+
+   app.get("/meals", async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || 6; // query ?limit=6
+    const meals = await mealsCollection.find().limit(limit).toArray();
+    res.send(meals);
+  } catch (err) {
+    res.status(500).send({ message: "Failed to fetch meals", error: err });
+  }
+});
+
+
+
+
+    console.log("MongoDB connected and chefsCollection ready");
+  } catch(err) {
+    console.error(err);
   }
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
