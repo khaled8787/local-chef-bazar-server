65e6cebd5cbc2b5032e5c45a339e9c7c4f8499fa
khaled8787 <khaledmahmud01737@gmail.com>
2025-12-07 01:35:54 +0600

    jwt funtionality addeded

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
 require('dotenv').config();
 
 const app = express();
@@ -24,6 +25,56 @@ async function run() {
     await client.connect();
     const db = client.db("LocalChefBazar");
     const mealsCollection = db.collection("meals");
+    const reviewsCollection = db.collection('reviews');
+
+
+
+    const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader)
+    return res.status(401).send({ message: "Unauthorized" });
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err)
+      return res.status(403).send({ message: "Forbidden" });
+
+    req.decoded = decoded;
+    next();
+  });
+};
+
+console.log("JWT_SECRET =", process.env.JWT_SECRET);
+
+
+
+    app.post("/jwt", (req, res) => {
+  const email = req.body.email;
+
+  const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.send({ token });
+});
+
+    // Latest 6 reviews for homepage
+app.get("/home-reviews", async (req, res) => {
+  try {
+    const reviews = await reviewsCollection
+      .find()
+      .sort({ date: -1 })
+      .limit(6)
+      .toArray();
+
+    res.send(reviews);
+  } catch (err) {
+    res.status(500).send({ message: "Failed to fetch home reviews", error: err });
+  }
+});
+
 
    app.get("/meals", async (req, res) => {
   try {
